28c1ded5fe4cd95b82f1b9fa579d44d911251e4a
MashodRana <mashod0rana@gmail.com>
2023-05-11 23:54:03 +0600

    sale url fixed

--- sale/urls.py
+++ sale/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from sale import views
 
 urlpatterns = [
-    path('sale/', views.Sale.as_view(), name='products'),
+    path('sale/', views.SaleView.as_view(), name='products'),
 
 ]
