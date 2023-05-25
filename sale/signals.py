aa415bd0fe141f864e4fe7a1e8af5c81ffef8aff
MashodRana <mashod0rana@gmail.com>
2023-05-25 23:37:18 +0600

    signals update for sale model

--- sale/signals.py
+++ sale/signals.py
@@ -10,10 +10,10 @@ from sale.models import Sale
 def update_product_stock(sender, instance, created, **kwargs):
     if created:
         sale_details = json.loads(instance.details)
-        aa = []
+        to_update_stocks = []
         for obj in sale_details:
-            s = Stock.objects.get(id=int(obj["id"]))
-            s.quantity = s.quantity-obj['saleQuantity']
-            aa.append(s)
-        Stock.objects.bulk_update(aa,['quantity'])
+            stock = Stock.objects.get(id=int(obj["id"]))
+            stock.quantity = stock.quantity-obj['saleQuantity']
+            to_update_stocks.append(stock)
+        Stock.objects.bulk_update(to_update_stocks, ['quantity'])
 
