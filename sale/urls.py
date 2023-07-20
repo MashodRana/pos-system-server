49d8e786da5fb292b7c0bfc0e22022b4c929fcc1
MashodRana <mashod0rana@gmail.com>
2023-07-20 18:02:48 +0600

    sale detail view with get method

--- sale/urls.py
+++ sale/urls.py
@@ -4,5 +4,6 @@ from sale import views
 
 urlpatterns = [
     path('sale/', views.SaleView.as_view(), name='products'),
+    path('sale/<int:id>', views.SaleDetailView.as_view(), name='products'),
 
 ]
--- sale/views.py
+++ sale/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
@@ -20,3 +20,10 @@ class SaleView(APIView):
             sale = serializer.save()
             return Response(serializer.data, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class SaleDetailView(APIView):
+    def get(self, request, id, format=None):
+        sale = get_object_or_404(Sale, pk=id)
+        serializer = SaleSerializer(sale)
+        return Response(serializer.data, status=HTTP_200_OK)
