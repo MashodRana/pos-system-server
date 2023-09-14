e3cae77db5d24626368f89434bdd349d22a96c7d
MashodRana <mashod0rana@gmail.com>
2023-09-14 18:31:17 +0600

    [DONE] authentication with oauth

--- inventory_of_shop/urls.py
+++ inventory_of_shop/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('', include('common.urls')),
     path('', include('product.urls')),
     path('', include('supplier.urls')),
--- sale/views.py
+++ sale/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework import permissions, generics, serializers
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
 from sale.models import Sale
 from sale.serializers import SaleSerializer
@@ -9,6 +11,7 @@ from sale.serializers import SaleSerializer
 
 # Create your views here.
 class SaleView(APIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     def get(self, request, format=None):
         sales = Sale.objects.all()
         serializer = SaleSerializer(sales, many=True)
